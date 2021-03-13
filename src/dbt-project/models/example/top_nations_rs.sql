2f0759d96fa8705c3c825d92b56907ba208a4fec
w7024015 <45118461+w7024015@users.noreply.github.com>
2021-03-13 15:10:36 +0200

    Create top_nations_rs.sql
    my test

---
+++ src/dbt-project/models/example/top_nations_rs.sql
@@ -0,0 +1,15 @@
+/*
+   Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+   SPDX-License-Identifier: MIT-0
+*/
+
+
+/*
+    Find 3 nations with the most active customers
+*/
+
+create table top_nations_rs as
+select
+*
+from
+top_nations
